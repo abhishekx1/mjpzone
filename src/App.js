7d8f1edd7921f16aca9ba92cb64fc4f9862b851f
Abhishek Srivastav <abhisheksrivastavx@gmail.com>
2023-04-03 18:48:26 +0530

    Add Student Login Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import StudentsLogin from './components/student/StudentsLogin';
 
 function App() {
   return (
     <div className="App">
       <h1>MJPZone</h1>
+      <StudentsLogin />
     </div>
   );
 }
---
+++ src/components/student/StudentsLogin.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+function StudentsLogin() {
+    return (
+        <>
+            <p>Student Login Components</p>
+        </>
+    );
+};
+
+export default StudentsLogin;
