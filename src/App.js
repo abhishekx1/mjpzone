16ef608f9a1362d3272efb1ce8bdf358b7645f5e
Abhishek Srivastav <abhisheksrivastavx@gmail.com>
2023-04-05 20:15:46 +0530

    Student login page commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import FacultyLogin from './components/faculty/FacultyLogin';
 import StudentsLogin from './components/student/StudentsLogin';
 
 function App() {
   return (
     <div className="App">
       <StudentsLogin />
-      <FacultyLogin />
     </div>
   );
 }
--- src/components/student/StudentsLogin.js
+++ src/components/student/StudentsLogin.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import "./css/StudentLogin.css";
+import Logo from 'D:/MCA Major Project/mjpzone/src/images/loginImage/mjp-logo.png';
 
 function StudentsLogin() {
     return (
         <>
             <div className='login-container'>
-                <div>
-                    <img src="" alt="logo" />
+                <div className='login-left'>
+                    <h1>MJPZONE</h1>
+                    <img src={Logo} alt="logo" />
                 </div>
-                <h1>Login <span>Student/Parent</span></h1>
-                <a href="/">Click here for faculty login</a>
-                <form action="submit">
-                    <div>
-                        <input type="text" name="username" id="username" placeholder="Username" />
-                    </div>
-                    <div>
-                        <input type="password" name="password" id="password" placeholder="Password" />
-                    </div>
-                    <div>
-                        <input type="button" value="Login" />
-                    </div>
-                </form>
+                <div className='login-right'>
+                    <h1>Login <span>Student/Parent</span></h1>
+                    <a href="/">Click here for faculty login</a>
+                    <form action="submit">
+                        <div>
+                            <label htmlFor="username">ENROLLMENT/ROLL NO:</label>
+                            <input type="text" name="username" id="username" placeholder="ENROLLMENT / ROLL NO" />
+                        </div>
+                        <div>
+                            <label htmlFor="dob">DATE OF BIRTH:</label>
+                            <input type="datetime" name="dob" id="dob" placeholder='DD-MM-YYYY' />
+                        </div>
+                        <div>
+                            <button type="submit" class="submit">Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </>
     );
